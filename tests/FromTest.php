a9fed67a7e40333e7f80d05180d3e2a8d6632306
Marcio <marcio@adue.digital>
2020-07-08 08:40:26 -0300

    Lesson 6

--- tests/FromTest.php
+++ tests/FromTest.php
@@ -4,8 +4,6 @@
 namespace Tests;
 
 
-use Illuminate\Support\Facades\Session;
-
 class FromTest extends TestCase
 {
 
@@ -21,30 +19,23 @@ class FromTest extends TestCase
     /** @test */
     function renders_a_form_with_post_method()
     {
-
-        Session::start();
-
         $this->assertTemplateRenders(
-            sprintf('<form method="post"><input type="hidden" name="_token" value="%s"></form>', Session::token()),
+            sprintf('<form method="post">%s</form>', $this->csrfField()),
             '<x-form method="post"></x-form>'
         );
     }
 
-    /** @test */
-    function renders_a_form_with_put_method()
+    /**
+     * @test
+     * @testWith ["put"]
+     *           ["patch"]
+     *           ["delete"]
+     */
+    function renders_a_form_with_put_method($method)
     {
         $this->assertTemplateRenders(
-            '<form method="post"><input type="hidden" name="_method" value="put"></form>',
-            '<x-form method="put"></x-form>'
-        );
-    }
-
-    /** @test */
-    function renders_a_form_with_patch_method()
-    {
-        $this->assertTemplateRenders(
-            '<form method="post"><input type="hidden" name="_method" value="patch"></form>',
-            '<x-form method="patch"></x-form>'
+            sprintf('<form method="post">%s<input type="hidden" name="_method" value="%s"></form>', $this->csrfField(), $method),
+            sprintf('<x-form method="%s"></x-form>', $method)
         );
     }
 
@@ -63,5 +54,12 @@ class FromTest extends TestCase
     protected function removeExtraSpaces(string $html)
     {
         return trim(preg_replace('/\s{2,}/', '', $html));
+    }
+
+    protected function csrfField()
+    {
+        $this->startSession();
+
+        return sprintf('<input type="hidden" name="_token" value="%s">', $this->app['session']->token());
     }
 }
