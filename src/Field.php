4edc061f9772d59d6558783f7aab1efe70349317
Marcio <marcio@adue.digital>
2020-07-11 00:39:16 -0300

    Working with config file

--- src/Field.php
+++ src/Field.php
@@ -3,6 +3,7 @@
 
 namespace Adue;
 
+use Illuminate\Config\Repository;
 use Illuminate\View\Component;
 
 class Field extends Component
@@ -11,10 +12,30 @@ class Field extends Component
      * @var String
      */
     public $name;
+    /**
+     * @var bool
+     */
+    public $required;
+    /**
+     * @var Repository
+     */
+    private $config;
 
-    public function __construct(String $name)
+    public function __construct(Repository $config, String $name, $required = false)
     {
         $this->name = $name;
+        $this->required = $required;
+        $this->config = $config;
+    }
+
+    public function highlightsRequirement()
+    {
+        return $this->config->get('form.highlights_requirement') === 'required' && $this->required;
+    }
+
+    public function highlightsOptional()
+    {
+        return $this->config->get('form.highlights_requirement') === 'optional' && ! $this->required;
     }
 
     public function render()
